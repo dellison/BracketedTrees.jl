ec358493a1f73be54e9f113528a5ebe66bda356e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-01-08 23:42:20 +0000

    build based on 426830e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BracketedTrees.jl-1","page":"Home","title":"BracketedTrees.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"todo lol","category":"page"}]
+}
